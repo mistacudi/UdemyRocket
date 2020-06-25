1cf5fffe4ec9f5bd0c41e9f8654002c64d3ec8f6
DESKTOP-Mistacudi <mistacudi@gmail.com>
2020-06-25 16:34:38 +0400

    Добавлен звук, элементы управления

---
+++ Assets/Rocket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Стартуем
+        {
+            rb.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) //Вращаемся влево
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))//Вращаемся вправо
+        {
+            transform.Rotate(-Vector3.forward); ;
+        }
+    }
+}
